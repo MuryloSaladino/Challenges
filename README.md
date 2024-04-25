363f6d0bceb147da335150a46fc5a5ed63ef398c
MuryloSaladino <murylosaladino@gmail.com>
2024-04-25 16:32:39 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Challenges
+
+I've realized i need to practice my logic, so this is my atempt to complete at least one challenge per day.
+
+### Challenges sources:
+- APAS (from Google PlayStore)
--- apas/AddTwoNumbers.java
+++ apas/AddTwoNumbers.java
@@ -2,16 +2,6 @@ package apas;
 
 import java.util.LinkedList;
 
-// You are given two non-empty linked lists representing two non-negative integers.
-// The digits are stored in reverse order and each of their nodes contain a single digit.
-// Add the two numbers and return it as a linked list.
-// You may assume the two numbers do not contain any leading zero, except the number zero itself.
-
-//Example:
-// Input: (2 -> 4 -> 3), (5 -> 6 -> 4)
-// Output: (7 -> 0 -> 8)
-// Because 342 + 465 = 807
-
 public class AddTwoNumbers {
 
     

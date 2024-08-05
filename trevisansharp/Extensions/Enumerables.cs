beba7ebf033a45174c49fbfcca88969b51181865
MuryloSaladino <murylosaladino@gmail.com>
2024-08-05 15:55:41 -0300

    feat: to array

--- trevisansharp/Extensions/Enumerables.cs
+++ trevisansharp/Extensions/Enumerables.cs
@@ -2,32 +2,46 @@ public static class Enumerables
 {
     public static IEnumerable<T> Take<T>(this IEnumerable<T> input, int count)
     {
-        var iterator = input.GetEnumerator();
-        for (int i = 0; i < count && iterator.MoveNext(); i++)
+        var it = input.GetEnumerator();
+        for (int i = 0; i < count && it.MoveNext(); i++)
         {
-            yield return iterator.Current;
+            yield return it.Current;
         }
     }
 
     public static IEnumerable<T> Skip<T>(this IEnumerable<T> input, int count)
     {
-        var iterator = input.GetEnumerator();
-        for (int i = 0; iterator.MoveNext(); i++)
+        var it = input.GetEnumerator();
+        for (int i = 0; it.MoveNext(); i++)
         {
             if(i < count) continue;
 
-            yield return iterator.Current;
+            yield return it.Current;
         }
     }
 
     public static int Count<T>(this IEnumerable<T> input)
     {
-        var iterator = input.GetEnumerator();
+        var it = input.GetEnumerator();
         int total = 0;
-        while (iterator.MoveNext())
+        while (it.MoveNext())
         {
             total++;
         }
         return total;
     }
+
+    public static T[] ToArray<T>(this IEnumerable<T> input)
+    {
+        T[] array = new T[input.Count()];
+        var it = input.GetEnumerator();
+
+        for (int i = 0; i < array.Length && it.MoveNext(); i++)
+        {
+            array[i] = it.Current;
+        }
+        return array;
+    }
+
+
 }
